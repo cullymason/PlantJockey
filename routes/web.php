a30dc4080bbc5823b5d40a840e8ad1653fefbc48
Cully Mason <cully@hey.com>
2021-05-03 18:30:16 -0400

    moved home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CellController;
+use App\Http\Controllers\CellPlantController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PlantController;
 use App\Http\Controllers\TrayController;
 use Illuminate\Support\Facades\Route;
@@ -16,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route('/', HomeController::class);
 
 Route::get('trays', [TrayController::class,'index'])->middleware('auth')->name('trays.index');
 
@@ -31,12 +31,12 @@ Route::get('plants', [PlantController::class,'index'])->middleware('auth')->name
 Route::get('plants/{plant}', [PlantController::class,'show'])->middleware('auth')->name('plants.show');
 Route::get('plants/{plant}/edit', [PlantController::class,'edit'])->middleware('auth')->name('plants.edit');
 Route::put('plants/{plant}', [PlantController::class,'update'])->middleware('auth')->name('plants.update');
-
 Route::post('plants', [PlantController::class,'store'])->middleware('auth')->name('plants.store');
 
 Route::get('cells/{cell}',[CellController::class,'show'])->middleware('auth')->name('cells.show');
 Route::put('cells/{cell}',[CellController::class,'update'])->middleware('auth')->name('cells.update');
-Route::post('cells/{cell}/plant',[\App\Http\Controllers\CellPlantController::class,'create'])->middleware('auth')->name('cell.plant.create');
-Route::delete('cells/{cell}/plant',[\App\Http\Controllers\CellPlantController::class,'delete'])->middleware('auth')->name('cell.plant.delete');
+
+Route::post('cells/{cell}/plant',[CellPlantController::class,'create'])->middleware('auth')->name('cell.plant.create');
+Route::delete('cells/{cell}/plant',[CellPlantController::class,'delete'])->middleware('auth')->name('cell.plant.delete');
 
 require __DIR__.'/auth.php';
