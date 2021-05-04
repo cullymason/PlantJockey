d9b985fe5d28bf2dc543060ebc3409dd137d07cf
Cully Mason <cully@hey.com>
2021-05-04 16:50:39 -0400

    bladeui in production should work

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -2,12 +2,12 @@
     <x-slot name="head">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         @livewireStyles
-        @bukStyles
+        @bukStyles(true)
     </x-slot>
     <x-main-nav />
     <div class="container bg-gradient-to-b from-gray-50 to-white mx-auto shadow-lg p-6 rounded-lg mt-6">
         {{$slot}}
     </div>
     @livewireScripts
-    @bukScripts
+    @bukScripts(true)
 </x-html>
